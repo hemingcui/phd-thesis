f2873333f2395b3647348825ded0a5f9f5ed7f1b
heming <heming@heming.(none)>
2014-12-22 16:19:16 -0500

    Refined part of peregrine figures.

--- peregrine/code/input-race.cpp
+++ peregrine/code/input-race.cpp
@@ -1,21 +1,16 @@
-// detect input-dependent races, and add involved dynamic instruction
-// instances to @slicing_targets@ used by the inter-thread step. @r1@ and
-// @r2@ are two concurrent regions
+// detect input-dependent races, and add involved dynamic instruction instances to @slicing_targets@
+// used by the inter-thread step. @r1@ and @r2@ are two concurrent regions
 void detect_input_dependent_races(r1, r2) {
-  // iterate through all instruction pairs in r1, r2
-  for (i1, i2) in (r1, r2) {
+  for (i1, i2) in (r1, r2) { // iterate through all instruction pairs in r1, r2
     if (neither i1 nor i2 is a branch instruction) {
-      if(mayrace(i1, i2)) {
-        slicing_targets.add(i1); // add @i1@ to slicing targets
-        slicing_targets.add(i2); // add @i2@ to slicing targets
-      }
+      if(mayrace(i1, i2))
+        { slicing_targets.add(i1); slicing_targets.add(i2); } // add @i1@ and @i2@ to slicing targets
     } else if (exactly one of i1, i2 is a branch instruction) {
       br = branch instruction in i1, i2;
       inst = the other instruction in i1, i2;
       nottaken = the not taken branch of br in the execution trace;
       if(mayrace_br(br, nottaken, inst)) {
-        // add the taken branch of @br@ to slicing targets
-        taken = the taken branch of br in trace;
+        taken = the taken branch of br in trace; // add the taken branch of @br@ to slicing targets
         slicing_targets.add_br(br, taken);
       }
     } else { // both @i1@, @i2@ are branches
@@ -28,25 +23,13 @@ void detect_input_dependent_races(r1, r2) {
     }
   }
 }
-// return true if instructions @i1@ and @i2@ may race
-bool mayrace(i1, i2) {
-  // query alias analysis
-  return mayalias(i1, i2) && ((i1 is a store) || (i2 is a store));
-}
-// return true if the not-taken branch of @br@ may race with @inst@
-bool mayrace_br(br, nottaken, inst) {
-  for i in (instructions in the nottaken branch of br) {
-    if(mayrace(i, inst))
-      return true;
-  }
-  return false;
-}
-// return true if the not-taken branch of @br1@ may race with the
-// not-taken branch of @br2@
+bool mayrace(i1, i2) { return mayalias(i1, i2) && ((i1 is a store) || (i2 is a store)); }
+bool mayrace_br(br, nottaken, inst) { // return true if the not-taken branch of @br@ may race with @inst@
+  for i in (instructions in the nottaken branch of br)
+    if(mayrace(i, inst)) return true;
+  return false; }
+// return true if the not-taken branch of @br1@ may race with the not-taken branch of @br2@
 bool mayrace_br_br(br1, nottaken1, br2, nottaken2) {
-  for inst in (instructions in the nottaken2 branch of br2) {
-    if(mayrace_br(br1, nottaken1, inst))
-      return true;
-  }
-  return false;
-}
+  for inst in (instructions in the nottaken2 branch of br2)
+    if(mayrace_br(br1, nottaken1, inst)) return true;
+  return false; }
